ccb6b264b1f0f1b303e2b433e4c8898e7e702ab7
ABHINAV M M <97299219+Abhinavmohanan@users.noreply.github.com>
2022-11-30 15:10:51 +0530

    Add files via upload

--- Exp 6/quicksort.java
+++ Exp 6/quicksort.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 
 public class quicksort {
-    static void quickSort(String arr[], int low, int high) {
+    static void swap(String arr[], int i, int j) {
+        String temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void quickSort(String arr[], int low, int high) {
         int p;
         if (low < high) {
             p = partition(arr, low, high);
@@ -11,41 +18,40 @@ public class quicksort {
     }
 
     static int partition(String arr[], int low, int high) {
-        String pivot, temp;
+        String pivot;
         pivot = arr[high];
 
-        int i = -1;
+        int i = low - 1;
         for (int j = low; j < high; j++) {
-            if (arr[j].compareTo(pivot) == -1) {
+            if (arr[j].compareToIgnoreCase(pivot) < 0) {
                 i++;
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                swap(arr, i, j);
             }
         }
-        temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
+        swap(arr, i + 1, high);
         return i + 1;
     }
 
     static void display(String arr[], int n) {
         for (int i = 0; i < n; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
+        System.out.println("");
     }
 
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         System.out.println("Enter the number of names");
         int n = read.nextInt();
+        read.nextLine();
         String[] arr = new String[n];
         System.out.println("Enter the names");
         for (int i = 0; i < n; i++) {
             arr[i] = read.nextLine();
         }
-        quickSort(arr, 0, n);
+        quickSort(arr, 0, n - 1);
         display(arr, n);
+        read.close();
 
     }
 }
